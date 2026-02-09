a25709a2d44ea256e636d45b8309aabe7228e23c
dhindalecio <dhindalecio@iesrafaelalberti.es>
2026-02-09 12:49:36 +0100

    Ejercicio de prueba dentro de nuestro proyecto

---
+++ js/apps.js
@@ -0,0 +1,7 @@
+
+
+//quiero añadir un script antes de mi footer, a ese div le añado una clase que tenga padding, margin, un background y el color y textplaile y
+//width 100% le quiero añadir a travas de js un mensaje de subcripcion y al final del contenido le añadws un enlace de ver mas
+
+
+const 
